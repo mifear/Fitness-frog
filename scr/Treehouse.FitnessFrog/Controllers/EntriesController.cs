8308d23756669d0b3b3a4b3abbd50f4339ceca26
Mike <mifear9@gmail.com>
2018-01-03 12:08:21 -0800

    Updated the Add action method to capture POST data.

--- scr/Treehouse.FitnessFrog/Controllers/EntriesController.cs
+++ scr/Treehouse.FitnessFrog/Controllers/EntriesController.cs
@@ -9,65 +9,77 @@ using Treehouse.FitnessFrog.Models;
 
 namespace Treehouse.FitnessFrog.Controllers
 {
-    public class EntriesController : Controller
+  public class EntriesController : Controller
+  {
+    private EntriesRepository _entriesRepository = null;
+
+    public EntriesController()
     {
-        private EntriesRepository _entriesRepository = null;
-
-        public EntriesController()
-        {
-            _entriesRepository = new EntriesRepository();
-        }
-
-        public ActionResult Index()
-        {
-            List<Entry> entries = _entriesRepository.GetEntries();
-
-            // Calculate the total activity.
-            double totalActivity = entries
-                .Where(e => e.Exclude == false)
-                .Sum(e => e.Duration);
-
-            // Determine the number of days that have entries.
-            int numberOfActiveDays = entries
-                .Select(e => e.Date)
-                .Distinct()
-                .Count();
-
-            ViewBag.TotalActivity = totalActivity;
-            ViewBag.AverageDailyActivity = (totalActivity / (double)numberOfActiveDays);
-
-            return View(entries);
-        }
-
-        public ActionResult Add()
-        {
-          return View();
-        }
-
-        [ActionName("Add"), HttpPost]
-        public ActionResult AddPost()
-            {
-                return View();
-            }
-
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-
-            return View();
-        }
-
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-
-            return View();
-        }
+      _entriesRepository = new EntriesRepository();
     }
+
+    public ActionResult Index()
+    {
+      List<Entry> entries = _entriesRepository.GetEntries();
+
+      // Calculate the total activity.
+      double totalActivity = entries
+          .Where(e => e.Exclude == false)
+          .Sum(e => e.Duration);
+
+      // Determine the number of days that have entries.
+      int numberOfActiveDays = entries
+          .Select(e => e.Date)
+          .Distinct()
+          .Count();
+
+      ViewBag.TotalActivity = totalActivity;
+      ViewBag.AverageDailyActivity = (totalActivity / (double)numberOfActiveDays);
+
+      return View(entries);
+    }
+
+    public ActionResult Add()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Add(string date, string activityId, string duration,
+                                string intensity, string exclude, string notes)
+    {
+      //string date = Request.Form["Date"];
+      //This line above will pull the date value from the form, but instead we are using
+      //parameters in the method signiture to pull the values from the form.
+
+      ViewBag.Date = date;
+      ViewBag.ActivityId = activityId;
+      ViewBag.Duration = duration;
+      ViewBag.Intensity = intensity;
+      ViewBag.Exclude = exclude;
+      ViewBag.Notes = notes;
+
+      return View();
+    }
+
+    public ActionResult Edit(int? id)
+    {
+      if (id == null)
+      {
+        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+      }
+
+      return View();
+    }
+
+    public ActionResult Delete(int? id)
+    {
+      if (id == null)
+      {
+        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+      }
+
+      return View();
+    }
+  }
 }
