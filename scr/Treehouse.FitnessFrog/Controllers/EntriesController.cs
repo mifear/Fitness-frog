630175a87c7e8e1e5d0d89531fdda72d5a0a9c28
Mike <mifear9@gmail.com>
2018-01-03 16:11:41 -0800

    Updated the EntriesController Add method parameter data types.

--- scr/Treehouse.FitnessFrog/Controllers/EntriesController.cs
+++ scr/Treehouse.FitnessFrog/Controllers/EntriesController.cs
@@ -45,8 +45,8 @@ namespace Treehouse.FitnessFrog.Controllers
     }
 
     [HttpPost]
-    public ActionResult Add(string date, string activityId, string duration,
-                                string intensity, string exclude, string notes)
+    public ActionResult Add(DateTime? date, int? activityId, double? duration,
+                                Entry.IntensityLevel? intensity, bool? exclude, string notes)
     {
       //string date = Request.Form["Date"];
       //This line above will pull the date value from the form, but instead we are using
